7443b40ecc881465dac126a8766a21d504a48a08
marco <marco.pyre@epsi.fr>
2025-07-10 09:57:44 +0200

    feat: google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,28 +27,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <meta
-          name="google-site-verification"
-          content="rzAe778nIs8FbIlJquuTddWn-Rv2-xbfGYWRg3uGTXw"
-        />
-        <Script id="gtm-script" strategy="afterInteractive">
-          {`
-            (function(w,d,s,l,i){
-              w[l]=w[l]||[];
-              w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
-              var f=d.getElementsByTagName(s)[0],
-                  j=d.createElement(s),
-                  dl=l!='dataLayer'?'&l='+l:'';
-              j.async=true;
-              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-              f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-N3WL2KF8');
-          `}
-        </Script>
-      </head>
-      {/* <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX"
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-3CQRTTN3DL"
         strategy="afterInteractive"
       />
       <Script id="ga-init" strategy="afterInteractive">
@@ -56,19 +36,12 @@ export default function RootLayout({
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', 'G-XXXXXXXXXX');
+          gtag('config', 'G-3CQRTTN3DL');
         `}
-      </Script> */}
+      </Script>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div
-          dangerouslySetInnerHTML={{
-            __html: `
-              <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N3WL2KF8" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-            `,
-          }}
-        />
         <LanguageProvider>
           <SplashScreen>{children}</SplashScreen>
         </LanguageProvider>
