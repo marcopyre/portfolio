5b5b718ab84f0814ae954b133d38e4812cb9a1bc
marco <marco.pyre@epsi.fr>
2025-07-10 19:50:40 +0200

    fix: compilation

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import SplashScreen from "../components/splash-screen";
 import { LanguageProvider } from "./i18n/language-provider";
-import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
